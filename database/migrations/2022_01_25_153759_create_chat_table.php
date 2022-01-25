6f13a254a8e84eb6a784aa6ed93a5956ab191db4
ElenaChichkina <74652478+ElenaChichkina@users.noreply.github.com>
2022-01-25 22:01:02 +0200

    Added1

---
+++ database/migrations/2022_01_25_153759_create_chat_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("author");
+            $table->text("message");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat');
+    }
+}
